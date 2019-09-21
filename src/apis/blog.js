af407a638df03d5bbc09ec3d1819ce956194bdf3
Jad Khoury <jad.khoury@foo-solutions.com>
2019-09-21 12:39:03 +0300

    Added env file

--- src/apis/blog.js
+++ src/apis/blog.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
 export default () => {
+    console.log('api: ', process.env.REACT_APP_BASE_URL);
      // grab auth user from local storage
     const authUser = localStorage.getItem('auth_user');
     
     // get user token from the store
     const token = authUser ? JSON.parse(authUser).token : '';
- 
+    
     return axios.create({
-        baseURL: 'http://localhost/blog/public/api',
+        baseURL: process.env.REACT_APP_BASE_URL || 'http://localhost/blog/public/api',
         headers: {
             Accept: 'application/json',
             Authorization: `Bearer ${token}`
